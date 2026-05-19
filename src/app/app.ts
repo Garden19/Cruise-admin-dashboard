5f842352ef858553f0052bac65475a4ca108b3ef
EdanR <edanjack1996@hotmail.com>
2026-05-20 00:22:33 +0100

    Created a login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./pages/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Cruise-admin-dashboard');
+
+  
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+  });
 
+  onSubmit() {
+        console.warn(this.loginForm.value);
+        alert("Welcome " + this.loginForm.value.userName);
+  }
 }
