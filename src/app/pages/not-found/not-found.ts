23b79feabe1000f0b4c80ca8f285b5406d788a0f
EdanR <edanjack1996@hotmail.com>
2026-05-20 22:18:43 +0100

    Removed the navbar from the not found page and added it to the root

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -4,7 +4,7 @@ import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink, Navbar],
+  imports: [RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
