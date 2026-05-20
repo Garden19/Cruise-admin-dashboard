b8a4e7e6667bac29540b240d7ea0eccfd8e1bed1
EdanR <edanjack1996@hotmail.com>
2026-05-20 20:43:55 +0100

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
+import { Enquiries } from './pages/enquiries/enquiries';
+import { EditEnquiry } from './pages/edit-enquiry/edit-enquiry';
+import { AddEnquiry } from './pages/add-enquiry/add-enquiry';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: Dashboard, title: 'Dashboard' },
+    { path: 'enquiries', component: Enquiries, title: 'Enquiries' },
+    { path: 'editEnquiry', component: EditEnquiry, title: 'Edit Enquiry' },
+    { path: 'addEnquiry', component: AddEnquiry, title: 'Add Enquiry' },
+    { path: '', pathMatch: 'full' ,component: Login, title: 'Login' },
+    { path: '**', component: NotFound }
+];
