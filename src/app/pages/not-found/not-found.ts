d0d5a43b49f18f10a8a7f6c3539ee12ea836b8f3
EdanR <edanjack1996@hotmail.com>
2026-05-20 22:07:45 +0100

    Added styling to the not found page

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink],
+  imports: [RouterLink, Navbar],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
