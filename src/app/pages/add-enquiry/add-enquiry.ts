bc6b036711148dfce62240ca24330ad91e0d2ba9
EdanR <edanjack1996@hotmail.com>
2026-05-25 22:42:36 +0100

    Added a form to the Add enquiry page.

--- src/app/pages/add-enquiry/add-enquiry.ts
+++ src/app/pages/add-enquiry/add-enquiry.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from "@angular/forms";
+import { NewEnquiry } from '../../models/newEnquiry';
 
 @Component({
   selector: 'app-add-enquiry',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-enquiry.html',
   styleUrl: './add-enquiry.css',
 })
 export class AddEnquiry {
 
+  addEnquiry = new FormGroup({
+    customerName: new FormControl('', Validators.required),
+    destination: new FormControl('', Validators.required),
+    status: new FormControl('', Validators.required),
+    budget: new FormControl(0, Validators.required),
+    nights: new FormControl(0, Validators.required),
+    travelDate: new FormControl('', Validators.required),
+    imageURL: new FormControl('', Validators.required),
+  })
+
+  onAddEnquiry() {
+    if(this.addEnquiry.invalid) {
+      return;
+    }
+
+    const newEnquiry: NewEnquiry = {
+      customerName: this.addEnquiry.value.customerName ?? '',
+      destination: this.addEnquiry.value.destination ?? '',
+      status: this.addEnquiry.value.status ?? '',
+      budget: this.addEnquiry.value.budget ?? 0,
+      nights: this.addEnquiry.value.nights ?? 0,
+      travelDate: this.addEnquiry.value.travelDate ?? '',
+      imageURL: this.addEnquiry.value.imageURL ?? ''
+  }
+  console.log(newEnquiry)
+  }
 }
