289b5f1c0e0487e3e0e195d95b35f3557c170ee3
EdanR <edanjack1996@hotmail.com>
2026-05-27 20:55:52 +0100

    Added logic so that the fake data is collected from the JSON, ready for new data to be added.

--- src/app/services/enquiry-service.ts
+++ src/app/services/enquiry-service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { NewEnquiry } from '../models/newEnquiry';
+import data from '../data/enquiries.json'
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnquiryService {
 
-  private enquiries: NewEnquiry[] = [];
+  private enquiries: NewEnquiry[] = data.enquiries;
 
   getEnquiries(): NewEnquiry[] {
     return this.enquiries;
