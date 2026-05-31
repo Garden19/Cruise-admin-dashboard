26c9c14895c60f5e62d1f4ca599f73369c0acd40
EdanR <edanjack1996@hotmail.com>
2026-06-01 00:08:18 +0100

    Refactored EnquiryService to use BehaviorSubject for reactive state updates.

---
+++ src/app/services/EnquiryService/enquiry-service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { NewEnquiry } from '../../models/newEnquiry';
+import data from '../../data/enquiries.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EnquiryService {
+  private enquiriesSubject = new BehaviorSubject<NewEnquiry[]>(data.enquiries);
+
+  enquiries$ = this.enquiriesSubject.asObservable();
+
+  addEnquiry(enquiry: NewEnquiry): void {
+    const currentEnquiries = this.enquiriesSubject.value;
+    const updatedEnquiries = [...currentEnquiries, enquiry];
+
+    this.enquiriesSubject.next(updatedEnquiries);
+  }
+
+  removeEnquiry(enquiryToRemove: NewEnquiry): void {
+    const currentEnquiries = this.enquiriesSubject.value;
+
+    const updatedEnquiries = currentEnquiries.filter((enquiry) => {
+      return enquiry !== enquiryToRemove;
+    });
+
+    this.enquiriesSubject.next(updatedEnquiries);
+  }
+
+  updateStatus(enquiryToUpdate: NewEnquiry, newStatus: string): void {
+    const currentEnquiries = this.enquiriesSubject.value;
+
+    const updatedEnquiries = currentEnquiries.map((enquiry) => {
+      if (enquiry === enquiryToUpdate) {
+        return { ...enquiry, status: newStatus };
+      }
+      return enquiry;
+    });
+
+    this.enquiriesSubject.next(updatedEnquiries);
+  }
+}
